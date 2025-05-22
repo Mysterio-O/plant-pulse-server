c9f72386f026c1d9a4b87ea273d3c80b980bc1d0
Mysterio-O <skrabbi.019@gmail.com>
2025-05-22 11:50:25 +0600

    added data post and data get and data get through id methods successfully

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
 
@@ -26,7 +29,30 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+
+        const plantCollection = client.db('plantsDB').collection('plants');
+
+        app.post('/plants', async (req, res) => {
+            const data = req.body;
+            const result = await plantCollection.insertOne(data);
+            res.send(result);
+        })
+
+        app.get('/plants', async (req, res) => {
+            const result = await plantCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/plants/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await plantCollection.findOne(query);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
