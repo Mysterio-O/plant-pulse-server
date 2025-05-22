9bfbbeeb3e98d41b511086874e39607968701079
Mysterio-O <skrabbi.019@gmail.com>
2025-05-22 13:49:56 +0600

    added delete functionality

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/plants/:id', async (req, res)=> {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const result = await plantCollection.deleteOne(filter);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
